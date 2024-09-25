402dd812b8b3d19d27fb4aec7538e68f63ca1a8f
Ulisses <ulisses1991@gmail.com>
2024-09-24 22:19:58 -0300

    aula 14

--- Basics/aula12.py
+++ Basics/aula12.py
@@ -5,6 +5,8 @@ imc = peso / (altura * altura)
 
 # IMC = peso / (altura x altura)
 
-
-
 print('O', nome, 'tem', altura, 'de altura, pesa ', peso, 'quilos e seu imc é ', imc)
+
+# Uma dica sobre f-strings
+
+print(f'O {nome} tem {altura} de altura, pesa {peso} quilos e seu imc é {imc:.2f}')
