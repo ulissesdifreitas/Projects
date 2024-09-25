fa24f9fb19b624ab75ebc5b721cee2cdf56ab0c1
Ulisses <ulisses1991@gmail.com>
2024-09-24 22:03:18 -0300

    aula 12

---
+++ Basics/aula10.py
@@ -0,0 +1,7 @@
+concatenacao = "A" + "B" + "C"
+print(concatenacao)
+
+a_dez_vezes = "A" * 10
+tres_vezes_ulisses = 3 * "Ulisses"
+print(a_dez_vezes)
+print(tres_vezes_ulisses)
---
+++ Basics/aula12.py
@@ -0,0 +1,10 @@
+nome = 'Ulisses'
+peso = 120
+altura = 1.75
+imc = peso / (altura * altura) 
+
+# IMC = peso / (altura x altura)
+
+
+
+print('O', nome, 'tem', altura, 'de altura, pesa ', peso, 'quilos e seu imc é ', imc)
