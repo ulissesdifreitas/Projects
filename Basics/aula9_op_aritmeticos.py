bc249ee6b273eb9a4f70caa15b049d77bd1193c5
Ulisses <ulisses1991@gmail.com>
2024-09-24 21:46:54 -0300

    aula 9

---
+++ Basics/aula9_op_aritmeticos.py
@@ -0,0 +1,28 @@
+
+# Operadores aritméticos
+
+adicao = 10 + 10
+print('adição', adicao)
+
+subtracao = 10 - 5
+print('subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('multiplição', multiplicacao)
+
+divisao = 10 / 2.2
+print('divisão', divisao)
+
+divisao_inteira = 10 // 2.2
+print('divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2  # resto de divisão
+print('Módulo', modulo)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
