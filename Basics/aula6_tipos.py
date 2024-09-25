57f878e1597cda28bf053823d05dd72a92af955d
Ulisses <ulisses1991@gmail.com>
2024-09-24 21:29:21 -0300

    aula 7

---
+++ Basics/aula6_tipos.py
@@ -0,0 +1,23 @@
+
+
+#Tipos
+
+# type() # Usado para identificar tipos de dados
+
+
+print(type(4.5)) #Float
+
+print(type(5))   #Int
+
+print(type("5")) # String, str
+
+print(type(True)) # Booleano, bool  
+
+
+# converção de tipos, coerção
+
+print(1) # int
+
+print(str(1)) # str
+
+print(type(1), type(str(1))) # conderinfo tipos
---
+++ Basics/aula7_variaveis.py
@@ -0,0 +1,14 @@
+
+# variaveis
+# use snake case ex. nome_variavel = expressao
+
+nome_completo = "Ulisses Dias Freitas"
+idade = 33
+maior_de_idade = idade >= 18
+soma_dois_mais_dois = 2 + 2
+
+print(nome_completo, soma_dois_mais_dois)
+
+print("Nome: ", nome_completo, "Idade: ", idade)
+print("É maior?", maior_de_idade)
+
